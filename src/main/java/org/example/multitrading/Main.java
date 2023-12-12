0a544d6c0be20150acc095f8e8f9f0030fbc33b0
david <davit.davtyan84@gmail.com>
2023-12-12 18:12:50 +0400

    temp

--- src/main/java/org/example/multitrading/Main.java
+++ src/main/java/org/example/multitrading/Main.java
@@ -1,30 +1,22 @@
 package org.example.multitrading;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         Thread thread = new MyThread();
         Thread thread2 = new MyThread();
-        Thread thread1 =new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println(Thread.currentThread());
-                    MyClass.extracted();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+        Thread thread1 = new Thread(() -> {
+            System.out.println(Thread.currentThread());
+            try {
+                MyClass.extracted();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
+
         });
         thread.start();
         thread2.start();
         thread1.start();
-//        thread.join(7000);
-//        for (int i = 5; i >= 0; i--) {
-//            System.out.println(i);
-//            Thread.sleep(1000);
-//        }
-//        Thread.sleep(2000);
-//        System.out.println("start");
+
     }
 }
