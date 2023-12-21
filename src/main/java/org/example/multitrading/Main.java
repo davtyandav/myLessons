cdc0e7bfdaa9d16e9ff436b763b1ae4d1c84cbf5
david <davit.davtyan84@gmail.com>
2023-12-21 18:11:24 +0400

    temp

--- src/main/java/org/example/multitrading/Main.java
+++ src/main/java/org/example/multitrading/Main.java
@@ -1,22 +1,29 @@
 package org.example.multitrading;
 
 public class Main {
+
     public static void main(String[] args) {
+        Incrementer incrementer = new Incrementer();
 
-        Thread thread = new MyThread();
-        Thread thread2 = new MyThread();
-        Thread thread1 = new Thread(() -> {
-            System.out.println(Thread.currentThread());
-            try {
-                MyClass.extracted();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100000; i++) {
+                incrementer.increment();
             }
+        };
 
-        });
-        thread.start();
-        thread2.start();
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+
         thread1.start();
+        thread2.start();
 
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(incrementer.getCounter());
     }
+
 }
---
+++ src/main/java/org/example/multitrading/Starter.java
@@ -0,0 +1,40 @@
+package org.example.multitrading;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Starter {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor =
+            new ThreadPoolExecutor(3,
+                                   7,
+                                   1, TimeUnit.MICROSECONDS,
+                                   new LinkedBlockingDeque<>(2));
+
+        for (int i = 0; i < 10; i++) {
+            MyCallable task = new MyCallable();
+            threadPoolExecutor.submit(task);
+
+        }
+        threadPoolExecutor.shutdown();
+    }
+}
+
+class MyCallable implements Callable<Long> {
+
+    @Override
+    public Long call() {
+        try {
+            System.out.println("Thread  started " + Thread.currentThread().getId());
+            Thread.sleep(2000);
+            System.out.println("Thread  end " + Thread.currentThread().getId());
+
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+
+        return Thread.currentThread().getId();
+    }
+}
